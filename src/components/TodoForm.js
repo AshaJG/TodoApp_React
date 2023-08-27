2c7d08171d3d94d44a016729a6a9fef1f25c5113
AshaJG <54907036+AshaJG@users.noreply.github.com>
2023-08-27 15:00:40 -0400

    Working on Todo Form

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-export const TodoForm = () => {
+export const TodoForm = ({addTodo}) => {
+  const [value, setValue] = useState('')
+
+  // function responsible to capture the value of the state when we submit the form 
+  const handleSubmit = e => {
+    e.preventDefault(); //prevent default action 
+   addTodo(value)
+   //after we set the form , we want to clear the form 
+   setValue("")
+  }
+
   return (
-  <form className = 'TodoForm' >
+  <form className = 'TodoForm' onSubmit  = {handleSubmit}>
     <input type = "text" className = 'todo-input'
-    placeholder='What is the task today>'/>
+      value = {value} onChange = {(e) => setValue(e.target.value)}
+      placeholder='What is the task today?' 
+      />
     <button type = 'submit' className = 'todo-btn'> Add Task </button>
-
   </form>)
 }
